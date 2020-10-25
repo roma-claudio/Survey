4cb7dd20a4e5bdbdcfe0781fbaeccadd0bbec498
roma-claudio <roma.claudio.it@gmail.com>
2020-10-25 20:10:10 +0000

    Update - removed console log from server and updated readme file

--- README.md
+++ README.md
@@ -5,8 +5,6 @@ MySQL database
 
 ## How to use it
 
-Edit the username and password of your MySQL database in backend/server.js
-
 Start the backend
 
 $ cd .\backend\
--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,8 @@ var mysql = require('mysql');
 //Create connetion
 var con = mysql.createConnection({
     host: "localhost",
-    user: "user",
-    password: "user_password" //TODO change user name and password
+    user: "user", //TODO change user name
+    password: "user_password" //TODO change password
   });
 
   con.connect(function(err) {
@@ -16,7 +16,6 @@ var con = mysql.createConnection({
     //Create Database
     con.query("CREATE DATABASE IF NOT EXISTS survey_db", function (err, result) {
       if (err) throw err;
-      console.log("Database created");
 
       con = mysql.createConnection({
         host: "localhost",
@@ -28,12 +27,11 @@ var con = mysql.createConnection({
       //Create Question Table  
       con.query("CREATE TABLE IF NOT EXISTS questions (pk_question VARCHAR(255), question VARCHAR(255), fk_answer VARCHAR(255))", function (err, result) {
         if (err) throw err;
-        console.log("Table created");
 
         //Insert Questions
         con.query("INSERT INTO questions (pk_question, question, fk_answer) VALUES ('1', 'How often do you eat meat and dairy?','1'),('2','How big are your portions sizes?','2'),('3','How much food ends up wasted in your household?','3'),('4','How often do you eat avocados, asparagus, kiwi fruit or pineapples?','1')", function (err, result) {
             if (err) throw err;
-            console.log("1 record inserted");
+            
         });
 
       });
@@ -41,12 +39,11 @@ var con = mysql.createConnection({
       //Create Answer Table  
       con.query("CREATE TABLE IF NOT EXISTS answers (pk_answer VARCHAR(255), answer VARCHAR(255))", function (err, result) {
         if (err) throw err;
-        console.log("Table created");
 
         //Insert Answers
         con.query("INSERT INTO answers (pk_answer, answer) VALUES ('1','Daily'),('1','1 or 2 times'),('1','3+ times per week'),('1','Not at all'),('2','Smaller than average'),('2','Average'),('2','Larger than average'),('2','I''m not sure'),('3','None'),('3','1-5 plates per week'),('3','6-10 plates per week'),('3','More than 10 plates per week')", function (err, result) {
             if (err) throw err;
-            console.log("1 record inserted");
+
         });
 
       });
@@ -70,7 +67,6 @@ http.createServer(function (req, res) {
                 } else if (!result || result.length === 0) {
                     res.end('No data available');
                 } else {
-                    console.log(result.length);
                     var answer = [];
                     var question = result[0].question;
                     result.forEach(element => {
